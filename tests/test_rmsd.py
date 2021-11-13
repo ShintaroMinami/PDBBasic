5857ed76dc2ae03c4ad2ea6787f18dc86601be2d
ShintaroMinami <sminami@neoleukin.com>
2021-11-13 04:23:50 -0800

    add unittest

--- tests/test_rmsd.py
+++ tests/test_rmsd.py
@@ -1,21 +1,24 @@
 from unittest import TestCase
 import os
-import numpy as np
-from pdbbasic import readpdb, rmsd, kabsch
+import torch
+from einops import rearrange
+import pdbbasic as pdbb
 
 class TestBasicGeneration(TestCase):
 
     def setUp(self):
+        torch.set_default_dtype(torch.float64)
         input_file = os.path.dirname(__file__)+'/data/7bqd_notag.pdb'
-        self.loopmod0 = LoopModelingSimple()
-        self.loopmod = LoopModeling()
-        self.struct0 = ProteinBackbone(file=input_file)
-        self.pose0 = pyrosetta.pose_from_file(filename=input_file)
+        coord = pdbb.readpdb(input_file, backbone_only=True)[:,1]
+        self.ca_batch1 = rearrange(coord, '(b l) c -> b l c', b=20)
+        self.ca_batch2 = self.ca_batch1 + 10.0
     
-    def test_generate_without_pyrosetta(self):
-        struct1 = self.loopmod0.generate(self.struct0, start=68, goal=72)
-        xyz0 = self.struct0[69:73+1,0:4]
-        xyz1 = struct1[69:73+1,0:4]
-        rmsd = np.sqrt(np.mean(((xyz0 - xyz1)**2)))
-        print("\nRMSD for test loop generation w/o pyrosetta : {:.4f} [A]  ({}-{} in neo215)".format(rmsd, 69, 73))
-        self.assertLess(rmsd, 0.5) # rmsd < 0.5
+    def test_rmsd_numpy(self):
+        rmsd = pdbb.rmsd(self.ca_batch1, self.ca_batch2)
+        self.assertAlmostEqual(rmsd.max(), 0)
+    
+    def test_rmsd_torch(self):
+        ca_batch1 = torch.Tensor(self.ca_batch1)
+        ca_batch2 = torch.Tensor(self.ca_batch2)
+        rmsd = pdbb.rmsd(ca_batch1, ca_batch2)
+        self.assertAlmostEqual(rmsd.max().item(), 0)
