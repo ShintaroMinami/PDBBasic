0df40bdca3c84db8dade1668ef8069bc35f10c3d
ShintaroMinami <sminami@neoleukin.com>
2021-11-16 23:04:22 -0800

    add many_vs_many rmsd unittest

--- pdbbasic/__init__.py
+++ pdbbasic/__init__.py
@@ -1,3 +1,3 @@
 from .pdbio import readpdb, writepdb
-from .rmsd import rmsd, kabsch
+from .rmsd import rmsd, kabsch, rmsd_many_vs_one, rmsd_many_vs_many
 from .geometric import distance_matrix, torsion_angles
--- tests/test_rmsd.py
+++ tests/test_rmsd.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import os
+import pickle
 import torch
 from einops import rearrange
 import pdbbasic as pdbb
@@ -9,10 +10,17 @@ class TestBasicGeneration(TestCase):
     def setUp(self):
         torch.set_default_dtype(torch.float64)
         input_file = os.path.dirname(__file__)+'/data/7bqd_notag.pdb'
+        ref_numpy_pkl = os.path.dirname(__file__)+'/data/RmsdMat5x15_numpy.pkl'
+        ref_torch_pkl = os.path.dirname(__file__)+'/data/RmsdMat5x15_torch.pkl'
         coord = pdbb.readpdb(input_file, CA_only=True)
         self.ca_batch1 = rearrange(coord, '(b l) c -> b l c', b=20)
         self.ca_batch2 = self.ca_batch1 + 10.0
-    
+        with open(ref_numpy_pkl, 'rb') as f:
+            self.rmsdmat_ref_numpy = pickle.load(f)
+        with open(ref_torch_pkl, 'rb') as f:
+            self.rmsdmat_ref_torch = pickle.load(f)
+        torch.set_default_dtype(torch.float64)
+
     def test_rmsd_numpy(self):
         rmsd = pdbb.rmsd(self.ca_batch1, self.ca_batch2)
         self.assertAlmostEqual(rmsd.max(), 0)
@@ -21,6 +29,17 @@ class TestBasicGeneration(TestCase):
         ca_batch1 = torch.Tensor(self.ca_batch1)
         ca_batch2 = torch.Tensor(self.ca_batch2)
         rmsd = pdbb.rmsd(ca_batch1, ca_batch2)
-        self.assertAlmostEqual(rmsd.max().item(), 0)
+        self.assertLess(rmsd.max().item(), 0.00001)
 
+    def test_rmsd_many_vs_many_numpy(self):
+        ca_batch1 = self.ca_batch1[:5]
+        ca_batch2 = self.ca_batch1[5:]
+        rmat = pdbb.rmsd_many_vs_many(ca_batch1, ca_batch2)
+        self.assertAlmostEqual((self.rmsdmat_ref_numpy-rmat).max(), 0)
         
+    def test_rmsd_many_vs_many_torch(self):
+        ca_batch1 = torch.Tensor(self.ca_batch1[:5])
+        ca_batch2 = torch.Tensor(self.ca_batch1[5:])
+        rmat = pdbb.rmsd_many_vs_many(ca_batch1, ca_batch2)
+        self.assertLess((self.rmsdmat_ref_torch-rmat).max().item(), 0.00001)
+
